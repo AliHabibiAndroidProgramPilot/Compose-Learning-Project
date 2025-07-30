48126c0e9c84e38b00bd8d7567dd7082c94e0308
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-07-30 22:09:43 +0330

    Let's Rock!

---
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package info.alihabibi.composelearningproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+
+}
