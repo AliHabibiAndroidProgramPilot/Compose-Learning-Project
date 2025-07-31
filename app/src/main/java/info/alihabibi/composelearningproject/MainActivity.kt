b9fddbb89b3925ba60e56cd5b292252685ee7df7
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-07-31 22:41:59 +0330

    All About Buttons

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -1,22 +1,27 @@
 package info.alihabibi.composelearningproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -38,27 +43,17 @@ class MainActivity : ComponentActivity() {
                     fontSize = 24.sp,
                     color = colorResource(R.color.teal_700)
                 )
-                Text(
-                    modifier = Modifier
+                Button(
+                    { Toast.makeText(this@MainActivity, "test toast", Toast.LENGTH_LONG).show() },
+                    Modifier
+                        .padding(5.dp)
                         .fillMaxWidth()
-                        .background(Color.Red)
-                        .padding(30.dp),
-                    text = "Ali Habibi",
-                    fontStyle = FontStyle.Italic,
-                    fontSize = 20.sp,
-                    color = colorResource(R.color.white)
-                )
-                Text(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(colorResource(R.color.black))
-                        .padding(30.dp),
-                    text = "متن فارسی",
-                    textAlign = TextAlign.Right,
-                    fontStyle = FontStyle.Italic,
-                    fontSize = 20.sp,
-                    color = Color.Magenta
-                )
+                        .height(60.dp),
+                    shape = RoundedCornerShape(20.dp, 5.dp, 20.dp, 5.dp),
+                    colors = ButtonDefaults.buttonColors(Color.Black, Color.White)
+                ) {
+                    Text("Click", fontSize = 20.sp)
+                }
             }
         }
     }
