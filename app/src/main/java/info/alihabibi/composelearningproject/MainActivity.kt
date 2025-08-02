0e6c20386d48c9980145c25135ece7d0798d2718
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-02 23:00:38 +0330

    All About Images

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -5,20 +5,26 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices.PIXEL_7_PRO
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,6 +55,7 @@ class MainActivity : ComponentActivity() {
                 fontSize = 24.sp,
                 color = colorResource(R.color.teal_700)
             )
+
             Button(
                 { Toast.makeText(this@MainActivity, "test toast", Toast.LENGTH_LONG).show() },
                 Modifier
@@ -59,6 +66,22 @@ class MainActivity : ComponentActivity() {
                 colors = ButtonDefaults.buttonColors(Color.Black, Color.White)
             ) {
                 Text("Click", fontSize = 20.sp)
+            }
+
+            Surface(
+                Modifier
+                    .padding(20.dp, 10.dp)
+                    .background(Color.Black),
+                shape = CircleShape
+            ) {
+                Image(
+                    painterResource(R.drawable.android_kotlin),
+                    null,
+                    Modifier
+                        .width(300.dp)
+                        .height(250.dp),
+                    contentScale = ContentScale.Crop
+                )
             }
         }
     }
