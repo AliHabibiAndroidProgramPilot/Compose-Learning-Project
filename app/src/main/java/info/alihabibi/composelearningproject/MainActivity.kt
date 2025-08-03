84ce155b3af1c86ba4d3223a9a8dab45232e8973
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-03 17:14:43 +0330

    Row View Group

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -1,37 +1,26 @@
 package info.alihabibi.composelearningproject
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Devices.PIXEL_7_PRO
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,134 +37,37 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun SetContentAndPreview() {
-        Column(
+        Row(
             modifier = Modifier
+                .fillMaxSize()
                 .navigationBarsPadding()
                 .statusBarsPadding()
-                .background(Color.DarkGray)
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
+                .background(Color.Cyan),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             Text(
                 modifier = Modifier
-                    .width(150.dp)
-                    .height(60.dp)
-                    .background(Color.DarkGray),
-                text = "Ali, Welcome to Compose World",
-                fontWeight = FontWeight.ExtraBold,
+                    .width(140.dp)
+                    .padding(20.dp)
+                    .background(Color.DarkGray)
+                    .padding(20.dp),
+                text = "Ali 2",
                 fontSize = 24.sp,
-                color = colorResource(R.color.teal_700)
+                textAlign = TextAlign.Center,
+                color = colorResource(R.color.white)
             )
-
-            Button(
-                {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "test toast",
-                        Toast.LENGTH_LONG
-                    ).show()
-                },
-                Modifier
-                    .width(160.dp)
-                    .height(60.dp),
-                shape = RoundedCornerShape(20.dp, 5.dp, 20.dp, 5.dp),
-                colors = ButtonDefaults.buttonColors(Color.Black, Color.White)
-            ) {
-                Text("Click", fontSize = 20.sp)
-            }
-
-            Surface(
-                Modifier
-                    .padding(20.dp, 10.dp)
-                    .background(Color.Black),
-                shape = CircleShape
-            ) {
-                Image(
-                    painterResource(R.drawable.android_kotlin),
-                    null,
-                    Modifier
-                        .width(300.dp)
-                        .height(250.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Surface(
-                Modifier
-                    .padding(20.dp, 10.dp)
-                    .background(Color.Black),
-                shape = CircleShape
-            ) {
-                Image(
-                    painterResource(R.drawable.android_kotlin),
-                    null,
-                    Modifier
-                        .width(300.dp)
-                        .height(250.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Surface(
-                Modifier
-                    .padding(20.dp, 10.dp)
-                    .background(Color.Black),
-                shape = CircleShape
-            ) {
-                Image(
-                    painterResource(R.drawable.android_kotlin),
-                    null,
-                    Modifier
-                        .width(300.dp)
-                        .height(250.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Surface(
-                Modifier
-                    .padding(20.dp, 10.dp)
-                    .background(Color.Black),
-                shape = CircleShape
-            ) {
-                Image(
-                    painterResource(R.drawable.android_kotlin),
-                    null,
-                    Modifier
-                        .width(300.dp)
-                        .height(250.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Surface(
-                Modifier
-                    .padding(20.dp, 10.dp)
-                    .background(Color.Black),
-                shape = CircleShape
-            ) {
-                Image(
-                    painterResource(R.drawable.android_kotlin),
-                    null,
-                    Modifier
-                        .width(300.dp)
-                        .height(250.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Surface(
-                Modifier
-                    .padding(20.dp, 10.dp)
-                    .background(Color.Black),
-                shape = CircleShape
-            ) {
-                Image(
-                    painterResource(R.drawable.android_kotlin),
-                    null,
-                    Modifier
-                        .width(300.dp)
-                        .height(250.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
+            Text(
+                modifier = Modifier
+                    .width(140.dp)
+                    .padding(20.dp)
+                    .background(Color.DarkGray)
+                    .padding(20.dp),
+                text = "Ali",
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                color = colorResource(R.color.white)
+            )
         }
     }
 
