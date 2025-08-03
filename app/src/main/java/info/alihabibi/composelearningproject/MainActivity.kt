06afda5ec654b67bbf05f13e17b506486afd7826
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-03 21:06:29 +0330

    UI Changed With States. (Change Text On Button Clicks)

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -5,21 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_7_PRO
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,85 +41,55 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun SetContentAndPreview() {
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .navigationBarsPadding()
                 .statusBarsPadding()
-                .background(Color.Cyan)
-                .horizontalScroll(rememberScrollState()),
-            verticalAlignment = Alignment.CenterVertically,
+                .background(Color.Green),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            var isShowingName by remember { mutableStateOf(true) }
+            var name: String by remember {
+                mutableStateOf("Ali Habibi")
+            }
             Text(
-                modifier = Modifier
-                    .width(140.dp)
-                    .padding(20.dp)
+                name,
+                Modifier
+                    .fillMaxWidth(0.8f)
+                    .padding(10.dp, 20.dp)
                     .background(Color.DarkGray)
-                    .padding(20.dp),
-                text = "Ali 2",
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center,
-                color = colorResource(R.color.white)
+                    .padding(15.dp),
+                Color.White,
+                32.sp,
+                textAlign = TextAlign.Center
             )
-            Text(
-                modifier = Modifier
-                    .width(140.dp)
-                    .padding(20.dp)
-                    .background(Color.DarkGray)
-                    .padding(20.dp),
-                text = "Ali",
-                textAlign = TextAlign.Center,
-                fontSize = 24.sp,
-                color = colorResource(R.color.white)
-            )
-            Text(
-                modifier = Modifier
-                    .width(140.dp)
-                    .padding(20.dp)
-                    .background(Color.DarkGray)
-                    .padding(20.dp),
-                text = "Ali",
-                textAlign = TextAlign.Center,
-                fontSize = 24.sp,
-                color = colorResource(R.color.white)
-            )
-            Text(
-                modifier = Modifier
-                    .width(140.dp)
-                    .padding(20.dp)
-                    .background(Color.DarkGray)
-                    .padding(20.dp),
-                text = "Ali",
-                textAlign = TextAlign.Center,
-                fontSize = 24.sp,
-                color = colorResource(R.color.white)
-            )
-            Text(
-                modifier = Modifier
-                    .width(140.dp)
-                    .padding(20.dp)
-                    .background(Color.DarkGray)
-                    .padding(20.dp),
-                text = "Ali",
-                textAlign = TextAlign.Center,
-                fontSize = 24.sp,
-                color = colorResource(R.color.white)
-            )
-            Text(
-                modifier = Modifier
-                    .width(140.dp)
-                    .padding(20.dp)
-                    .background(Color.DarkGray)
-                    .padding(20.dp),
-                text = "Ali",
-                textAlign = TextAlign.Center,
-                fontSize = 24.sp,
-                color = colorResource(R.color.white)
-            )
+
+            Button(
+                {
+                    name = if (isShowingName)
+                        "Recomposition"
+                    else
+                        "Ali Habibi"
+                    isShowingName = !isShowingName
+                },
+                Modifier.padding(top = 28.dp),
+                colors = ButtonDefaults.buttonColors(
+                    Color.Magenta,
+                    Color.White
+                ),
+                shape = RoundedCornerShape(18.dp)
+            ) {
+                Text(
+                    "Click Here",
+                    fontSize = 22.sp
+                )
+            }
         }
+
     }
 
-    @Preview("Preview", widthDp = 450, heightDp = 900, showBackground = true)
+    @Preview("Preview", showSystemUi = true, device = PIXEL_7_PRO)
     @Composable
     fun EnablePreview() {
         SetContentAndPreview()
