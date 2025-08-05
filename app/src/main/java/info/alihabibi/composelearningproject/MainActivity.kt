c60960255c796ab493c53d1b25200e2c4f1b58e5
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-05 13:33:27 +0330

    Initializing Text Fields

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -4,28 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
@@ -41,20 +34,21 @@ class MainActivity : ComponentActivity() {
     fun SetContentAndPreview() {
         Column(
             Modifier
-                .padding(30.dp)
-                .width(350.dp)
-                .height(380.dp)
-                .background(
-                    Brush.linearGradient(listOf(Color.Cyan, Color.Magenta))
-                )
-                /*.background(
-                    Brush.horizontalGradient(
-                        Pair(0.3f, Color.Green),
-                        Pair(0.5f, Color.Yellow)
-                    )
-                )*/
+                .fillMaxSize()
+                .padding(10.dp)
+                .statusBarsPadding()
+                .navigationBarsPadding()
         ) {
+            var textFieldText by remember {
+                mutableStateOf(TextFieldValue())
+            }
 
+            TextField(
+                textFieldText,
+                {
+                    textFieldText = it
+                }
+            )
         }
     }
 
