de9556b7d6a98d63f334d44450992b981cea24c3
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-08 14:22:01 +0330

    Borders And Strokes

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -4,28 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
 
@@ -49,7 +48,9 @@ class MainActivity : ComponentActivity() {
                 null,
                 Modifier
                     .size(300.dp, 280.dp)
+                    // clip & border Must Have Same Shape!
                     .clip(CutCornerShape(20))
+                    .border(BorderStroke(12.dp, Color.Green), CutCornerShape(20))
                     .background(Color.Red),
                 contentScale = ContentScale.FillBounds
             )
@@ -58,7 +59,6 @@ class MainActivity : ComponentActivity() {
                 "Ali Habibi",
                 Modifier
                     .padding(top = 10.dp)
-                    .clip(RoundedCornerShape(35.dp))
                     .background(Color.Yellow)
                     .padding(15.dp),
                 Color.Black,
