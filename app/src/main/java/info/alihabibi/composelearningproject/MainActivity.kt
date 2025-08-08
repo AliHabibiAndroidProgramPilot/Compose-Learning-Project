d06c4a2de13efbc9fce1b42836e4e022c15915f9
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-08 14:43:43 +0330

    Clickable Icons And Texts

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -1,26 +1,25 @@
 package info.alihabibi.composelearningproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,24 +42,39 @@ class MainActivity : ComponentActivity() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painterResource(R.drawable.picsum),
+            Icon(
+                painterResource(R.drawable.ic_call),
                 null,
                 Modifier
-                    .size(300.dp, 280.dp)
-                    // clip & border Must Have Same Shape!
-                    .clip(CutCornerShape(20))
-                    .border(BorderStroke(12.dp, Color.Green), CutCornerShape(20))
-                    .background(Color.Red),
-                contentScale = ContentScale.FillBounds
+                    .size(62.dp)
+                    .padding(10.dp),
+                Color.Green
             )
+
+            IconButton(
+                {
+                    Toast.makeText(this@MainActivity, "Icon Button", Toast.LENGTH_SHORT).show()
+                },
+                Modifier
+                    .size(70.dp)
+                    .padding(10.dp)
+            ) {
+                Icon(
+                    Icons.Outlined.LocationOn,
+                    null,
+                    Modifier
+                        .size(42.dp)
+                        .padding(5.dp)
+                )
+            }
 
             Text(
                 "Ali Habibi",
                 Modifier
                     .padding(top = 10.dp)
-                    .background(Color.Yellow)
-                    .padding(15.dp),
+                    .clickable(true) {
+                        Toast.makeText(this@MainActivity, "Icon Button", Toast.LENGTH_SHORT).show()
+                    },
                 Color.Black,
                 26.sp
             )
