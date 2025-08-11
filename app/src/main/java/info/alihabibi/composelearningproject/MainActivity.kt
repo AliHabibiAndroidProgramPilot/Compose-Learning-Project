02715ec67b1f8dec28038b53a0fcf468237703d3
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-11 16:50:58 +0330

    Alert Dialog Colors And Icons

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -9,17 +9,22 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -42,46 +47,63 @@ class MainActivity : ComponentActivity() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            AlertDialog(
-                onDismissRequest = {
-
-                },
-                confirmButton = {
-                    Text(
-                        "Yes",
-                        Modifier.clickable {
-                            Toast.makeText(this@MainActivity, "YES", Toast.LENGTH_SHORT).show()
-                        },
-                        fontSize = 20.sp
-                    )
-                },
-                dismissButton = {
-                    Text(
-                        "No",
-                        Modifier.clickable {
-                            Toast.makeText(this@MainActivity, "NO", Toast.LENGTH_SHORT).show()
-                        },
-                        fontSize = 20.sp
-                    )
-                },
-                title = {
-                    Text(
-                        "Dialog Title",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        fontSize = 22.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                text = {
-                    Text(
-                        "Dialog Message is this and here so look it!",
-                        fontSize = 20.sp,
-                        fontFamily = FontFamily.Monospace,
-                    )
-                }
-            )
+            Dialog()
         }
+    }
+
+    @Composable
+    fun Dialog() {
+        AlertDialog(
+            onDismissRequest = {
+                true
+            },
+            confirmButton = {
+                Text(
+                    "Yes",
+                    Modifier.clickable {
+                        Toast.makeText(this@MainActivity, "YES", Toast.LENGTH_SHORT).show()
+                    },
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+            },
+            dismissButton = {
+                Text(
+                    "No",
+                    Modifier.clickable {
+                        Toast.makeText(this@MainActivity, "NO", Toast.LENGTH_SHORT).show()
+                    },
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+            },
+            title = {
+                Text(
+                    "Dialog Title",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            text = {
+                Text(
+                    "Dialog Message is this and here so look it!!",
+                    fontSize = 20.sp
+                )
+            },
+            containerColor = Color.DarkGray,
+            titleContentColor = Color.White,
+            textContentColor = Color.White,
+            iconContentColor = Color.Magenta,
+            icon = {
+                Icon(
+                    modifier = Modifier.size(35.dp),
+                    imageVector = Icons.TwoTone.Warning,
+                    contentDescription = null
+                )
+            }
+        )
     }
 
     /*@Preview("Preview", widthDp = 450, heightDp = 800, showBackground = true)
