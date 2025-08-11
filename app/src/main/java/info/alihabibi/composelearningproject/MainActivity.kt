73a7715770323c64e019a7747b961bb3e6392a98
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-11 23:29:54 +0330

    All About Radio Buttons And Related States More Moder Code

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -6,8 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,8 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -32,33 +36,37 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun SetContentAndPreview() {
-        val textStyle = TextStyle(
-            color = Color.Black,
-            fontSize = 16.sp
-        )
-        val radioButtons = listOf("Light Mode", "Night Mode", "System Default")
-        val selectedItem = remember {
-            mutableStateOf("Light Mode")
-        }
+        val themeOption = listOf("Light Mode", "Nigh Mode", "System Default")
+        val (selectedOption, onOptionSelected) = remember { mutableStateOf(themeOption[0]) }
         Row(
-            Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .padding(10.dp, 50.dp)
+                .selectableGroup(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            radioButtons.forEach { item ->
+            themeOption.forEach { text ->
                 Row(
+                    modifier = Modifier
+                        .weight(1f)
+                        .selectable(
+                            selected = (text == selectedOption),
+                            onClick = { onOptionSelected(text) },
+                            role = Role.RadioButton
+                        )
+                        .padding(horizontal = 8.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     RadioButton(
-                        (selectedItem.value == item),
-                        {
-                            selectedItem.value = item
-                        }
+                        selected = (text == selectedOption),
+                        onClick = null
                     )
+                    Spacer(Modifier.padding(8.dp))
                     Text(
-                        item,
-                        modifier = Modifier.padding(start = 10.dp),
-                        style = textStyle
+                        text = text,
+                        style = MaterialTheme.typography.bodyMedium.copy(fontSize = 16.sp)
                     )
                 }
             }
