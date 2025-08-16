3785454f94ece61ef731eed1e00f4ae0e98ab5fa
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-16 21:22:01 +0330

    Show Slider Current Range Number

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -4,34 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.fastRoundToInt
 
 class MainActivity : ComponentActivity() {
 
@@ -45,83 +34,39 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun SetContentAndPreview() {
-        var indicatorProgress by remember { mutableFloatStateOf(0.0f) }
-        val animatedProgress by animateFloatAsState(
-            targetValue = indicatorProgress,
-            animationSpec = tween(durationMillis = 2000, easing = LinearEasing),
-            label = "Progress AAnimation"
-        )
-        LaunchedEffect(Unit) {
-            indicatorProgress = 1f
+        val (onSliderStateValue, onSliderStateValueChanged) = remember {
+            mutableFloatStateOf(0.0f)
         }
+
         Column(
             modifier = Modifier
-                .fillMaxSize()
                 .statusBarsPadding()
-                .padding(25.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(20.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(70.dp),
-                color = Color.Green
-            )
-
-            Spacer(Modifier.padding(vertical = 30.dp))
-
-            LinearProgressIndicator(
-                color = Color.DarkGray,
-                strokeCap = StrokeCap.Round
-            )
-
-            Spacer(Modifier.padding(vertical = 30.dp))
-
-            CircularProgressIndicator(
-                progress = { 0.75f },
-                modifier = Modifier.size(70.dp),
-                color = Color.Gray
-            )
-            Spacer(Modifier.padding(vertical = 30.dp))
-
-            LinearProgressIndicator(
-                progress = { 0.20f },
-                color = Color.DarkGray,
-                strokeCap = StrokeCap.Round
-            )
-
-            Spacer(Modifier.padding(vertical = 30.dp))
-
-            LinearProgressIndicator(
-                progress = { animatedProgress },
-                color = Color.Blue
-            )
-
-            Spacer(Modifier.padding(vertical = 30.dp))
-
-            CircularProgressIndicator(
-                modifier = Modifier.size(50.dp),
-                progress = { animatedProgress },
-                color = Color.Magenta
-            )
-
-            Spacer(Modifier.padding(vertical = 10.dp))
-
-            OutlinedButton(
-                onClick = {
-                    indicatorProgress = if (indicatorProgress == 0f) 1f else 0f
+            Slider(
+                value = onSliderStateValue,
+                onValueChange = {
+                    onSliderStateValueChanged(it)
                 },
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp),
-                shape = RoundedCornerShape(10.dp),
-                border = BorderStroke(0.8.dp, Color.Black),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = Color.Black
-                )
-            ) {
-                Text("Re Progress")
-            }
+                    .fillMaxWidth(0.8f),
+                valueRange = (0.0f..100.0f)
+            )
+
+            Text(
+                onSliderStateValue.removeZero(onSliderStateValue),
+                color = Color.Black,
+                fontSize = 26.sp
+            )
         }
     }
+
+    private infix fun Float.removeZero(floatNumber: Float): String =
+        floatNumber.fastRoundToInt().toString()
+
 }
 
 /*@Preview("Preview", widthDp = 450, heightDp = 800, showBackground = true)
