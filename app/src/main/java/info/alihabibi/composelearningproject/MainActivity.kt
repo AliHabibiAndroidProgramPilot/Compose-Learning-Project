f50c53a12f7fc83410b40a235e4f897c163cbf49
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-17 15:38:59 +0330

    Basis Of Navigation Bar

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -1,56 +1,37 @@
 package info.alihabibi.composelearningproject
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Switch
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Face
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.util.fastRoundToInt
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var prefsManager: SharedPreferences
-
-    private val textColors = arrayListOf("Color Red", "Color Magenta", "Color Blue")
-
-    private val sliderStateValue = mutableFloatStateOf(20.0f)
-
-    private val switchStateValue = mutableStateOf(false)
-
-    private val radioStateValue = mutableStateOf(textColors.first())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        prefsManager = getSharedPreferences("SETTING_PREFS", MODE_PRIVATE)
-        sliderStateValue.floatValue = prefsManager.getFloat("FONT_SIZE", 20.0f)
-        switchStateValue.value = prefsManager.getBoolean("BOLD_TEXT", false)
-        radioStateValue.value = prefsManager.getString("APP_THEME", textColors.first())!!
         setContent {
             SetContentAndPreview()
         }
@@ -63,89 +44,41 @@ class MainActivity : ComponentActivity() {
                 .statusBarsPadding()
                 .padding(10.dp)
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Slider(
-                value = sliderStateValue.floatValue,
-                onValueChange = {
-                    sliderStateValue.floatValue = it
-                },
-                modifier = Modifier
-                    .fillMaxWidth(0.9f),
-                valueRange = (0.0f..60.0f)
-            )
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                textColors.forEach {
-                    Text(it, fontSize = 14.sp)
-                    RadioButton(
-                        selected = (radioStateValue.value == it),
-                        onClick = {
-                            radioStateValue.value = it
-                        }
-                    )
-                }
-            }
-
-            Switch(
-                checked = switchStateValue.value,
-                onCheckedChange = {
-                    switchStateValue.value = it
-                },
-                modifier = Modifier
-                    .scale(1.2f)
-                    .padding(vertical = 10.dp)
-            )
-
-            Text(
-                sliderStateValue.floatValue.removeZero(sliderStateValue.floatValue),
-                color = Color.Black,
-                fontSize = 26.sp
-            )
-
-            Text(
-                "Sample Text",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 40.dp),
-                color = when (radioStateValue.value) {
-                    textColors[0] -> Color.Red
-                    textColors[1] -> Color.Magenta
-                    textColors[2] -> Color.Blue
-                    else -> Color.Red
-                },
-                textAlign = TextAlign.Center,
-                fontFamily = FontFamily.SansSerif,
-                fontSize = sliderStateValue.floatValue.sp,
-                fontWeight = if (switchStateValue.value) FontWeight.ExtraBold else FontWeight.Normal
-            )
-
-            Button(
-                onClick = {
-                    prefsManager.edit().apply {
-                        putFloat("FONT_SIZE", sliderStateValue.floatValue)
-                        putBoolean("BOLD_TEXT", switchStateValue.value)
-                        putString("APP_THEME", radioStateValue.value)
-                        apply()
+            val navigationItems = listOf("Setting", "Profile", "Feed")
+            var navigationItemsState by remember { mutableIntStateOf(1) }
+            BottomAppBar {
+                NavigationBar {
+                    navigationItems.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            selected = (index == navigationItemsState),
+                            onClick = {
+                                navigationItemsState = index
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector = when (index) {
+                                        0 -> Icons.Rounded.Settings
+                                        1 -> Icons.Rounded.Face
+                                        2 -> Icons.Rounded.Notifications
+                                        else -> Icons.Rounded.Face
+                                    },
+                                    contentDescription = null
+                                )
+                            },
+                            label = {
+                                Text(
+                                    text = item,
+                                    fontSize = 16.sp
+                                )
+                            }
+                        )
                     }
-                },
-                modifier = Modifier.fillMaxWidth(0.9f).padding(vertical = 40.dp),
-                shape = RectangleShape
-            ) {
-                Text(
-                    "Save Settings",
-                    fontSize = 22.sp
-                )
+                }
             }
         }
     }
-
-    private infix fun Float.removeZero(floatNumber: Float): String =
-        floatNumber.fastRoundToInt().toString()
-
 }
 
 /*@Preview("Preview", widthDp = 450, heightDp = 800, showBackground = true)
