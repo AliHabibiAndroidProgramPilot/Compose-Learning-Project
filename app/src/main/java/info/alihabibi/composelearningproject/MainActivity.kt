e6f7b9ac3df24498f9614c1e6468d1a9af774466
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2025-08-22 14:25:40 +0330

    Using Weights Form Ordering Views

--- app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
+++ app/src/main/java/info/alihabibi/composelearningproject/MainActivity.kt
@@ -7,31 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Edit
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
 
@@ -43,66 +30,43 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun SetContentAndPreview() {
-        Scaffold(
-            topBar = {
-                CenterAlignedTopAppBar(
-                    modifier = Modifier.statusBarsPadding(),
-                    title = {
-                        Text(
-                            "Top App Bar",
-                            fontWeight = FontWeight.Bold
-                        )
-                    },
-                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                        containerColor = Color.LightGray
-                    )
-                )
-            },
-            bottomBar = {
-                BottomAppBar(
-                    modifier = Modifier.navigationBarsPadding(),
-                    containerColor = Color.DarkGray,
-                    contentColor = Color.White
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            "Bottom App Bar",
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-                }
-            },
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = {},
-                    shape = CircleShape,
-                    contentColor = Color.White,
-                    containerColor = Color.Black
-                ) {
-                    Icon(
-                        imageVector = Icons.Rounded.Edit,
-                        contentDescription = null
-                    )
-                }
-            },
-            floatingActionButtonPosition = FabPosition.End
-        ) { innerPadding ->
-            Column(
-                modifier = Modifier.padding(innerPadding),
-                horizontalAlignment = Alignment.CenterHorizontally
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(5.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    "Hello World!",
-                    modifier = Modifier.padding(15.dp),
-                    fontSize = 24.sp
-                )
+                Button(
+                    onClick = {},
+                    modifier = Modifier.weight(0.3f)
+                ) {
+                    Text("Click", fontSize = 20.sp)
+                }
+
+                Spacer(Modifier.width(15.dp))
+
+                Button(
+                    onClick = {},
+                    modifier = Modifier.weight(0.3f)
+                ) {
+                    Text("Click", fontSize = 20.sp)
+                }
+
+                Spacer(Modifier.width(15.dp))
+
+                Button(
+                    onClick = {},
+                    modifier = Modifier.weight(0.3f)
+                ) {
+                    Text("Click", fontSize = 20.sp)
+                }
             }
         }
     }
